2429e0d19c8a94b31f0868a65420cd75344b9ebd
Artem Lyksa <Artem@alexanders-macbook-air.local>
2018-01-12 10:26:08 +0200

    Initial Commit

---
+++ I_ntercom_Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I_ntercom_Example
+//
+//  Created by Artem Lyksa on 1/12/18.
+//  Copyright © 2018 Artem Lyksa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
